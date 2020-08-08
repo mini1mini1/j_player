c8c9b29f5a3c9597c93d90b7a17ddfe982a2063b
byunggil min <mini1mini1@naver.com>
2020-08-09 07:48:22 +0900

    Initial Commit

---
+++ j_player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  j_player
+//
+//  Created by Byungkil Min on 2020/08/09.
+//  Copyright © 2020 Byungkil Min. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
